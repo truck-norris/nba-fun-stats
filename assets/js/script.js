07881a0e4d3135904a19f4d461dfb36b06bb92fb
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2023-03-07 20:38:20 -0600

    cleaned up js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,56 @@
+// user types in team and selects from dropdown menu
+// team stats and a song linked to the team will appear
+// add eventListener to the form
+// capture the users search term
+// add search term to the request url
+// add api key
+// set up our fetch call
+// handle the response
+// loop over data 
+// render data to the page
+// user clicks reset button
+// song and team stats dissappear
 
-/*
-user types in team and selects from dropdown menu
-team stats and a song linked to the team will appear
-
-add eventListener to the form
-
-capture the users search term
-add search term to the request url
-  - add api key
- 
-
-  set up our fetch call
-  handle the response
-    loop over data 
-    render data to the page
-
-
-user clicks reset button
-song and team stats dissappear
-*/
 var searchInput = document.querySelector('#query-term')
 var statsForm = document.querySelector('#giphy-form')
 var imageContainer = document.querySelector('#giphy-images')
 
 function displayStats(event) {
-    event.preventDefault()
-//     var searchTerm = searchInput.value
-//     var apiKey = '4658ee7f44msh4e201b34602efeep1a45bcjsnb2ea38009195'
-//     var nbaUrl = 'https://api-nba-v1.p.rapidapi.com/seasons'
-//     var requestUrl = nbaUrl + '?api_key=' + apiKey + searchTerm
+  event.preventDefault()
+  var searchTerm = searchInput.value
+  var apiKey = '4658ee7f44msh4e201b34602efeep1a45bcjsnb2ea38009195'
+  var nbaUrl = 'https://api-nba-v1.p.rapidapi.com/seasons'
+  var requestUrl = nbaUrl + '?api_key=' + apiKey + searchTerm
 
-//     fetch(requestUrl)
-//     .then(function(response){
-//         return response.json()
-//     }).then(function(stats){
-//         console.log('here is data', stats.data)
+  fetch(requestUrl)
+  .then(function(response){
+    return response.json()
+  }).then(function(stats){
+    console.log('here is data', stats.data)
+    for (var i = 0; i < stats.data.length; i++){
+      var title = stats.data[i].title
+      var imageTag = document.createElement('p')
+      statsForm.appendChild(imageTag)
+    }
+  });
 
-//         for (var i = 0; i < stats.data.length; i++){
-//             var title = stats.data[i].title
-       
-//            var imageTag = document.createElement('p')
-//            statsForm.appendChild(imageTag)
-//         }
-//     })
-// NBA API
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '4658ee7f44msh4e201b34602efeep1a45bcjsnb2ea38009195',
-// 		'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
-// 	}
-// };
+  // NBA API
+  const options = {
+	  method: 'GET',
+	  headers: {
+		  'X-RapidAPI-Key': '4658ee7f44msh4e201b34602efeep1a45bcjsnb2ea38009195',
+		  'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+	  }
+  };
 
-// fetch('https://free-nba.p.rapidapi.com/players?page=0&per_page=25', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-// }
+  fetch('https://free-nba.p.rapidapi.com/players?page=0&per_page=25', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 
-// Music API
-}
+// YouTube API
+};
+
 statsForm.addEventListener('submit', displayStats);
 
 // Autocomplete widget
@@ -102,10 +93,10 @@ statsForm.addEventListener('submit', displayStats);
 // });
 
 // Clear Button
-// var clearBtn = document.querySelector('#clear-btn')
+var clearBtn = document.querySelector('#clear-btn')
 
-// function clearScreen() {
-//   location.reload();
-// };
+function clearScreen() {
+  location.reload();
+};
 
-// clearBtn.addEventListener('click', clearScreen)
+clearBtn.addEventListener('click', clearScreen);
