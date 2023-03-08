cc5bbb1ccc9406327c3ca18ed9ba20c68d8f3164
Jessie Galindo <121585545+MrMessyFace@users.noreply.github.com>
2023-03-07 20:55:10 -0600

    re-added youtube api

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,7 +48,15 @@ function displayStats(event) {
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
-// YouTube API
+  // YouTube API
+  function videos() {
+    console.log('it works')
+    // YT generic URL "https://www.googleapis.com/youtube/v3/search?q=something&key=YOUR_API_KEY&fields=items(id,snippet(title,thumbnails(default))&part=snippet" 
+    // Adrian's YT API key - AIzaSyD51ROkLO7Tj0gCUhdXzYzD-EmqGUCV1iI   
+    var youtubeUrl = "https://www.googleapis.com/youtube/v3/search?q="
+    var youtubeApi = "&limit=3&key=AIzaSyD51ROkLO7Tj0gCUhdXzYzD-EmqGUCV1iI"
+    var youtubeVid = youtubeUrl+youtubeApi
+  };
 };
 
 statsForm.addEventListener('submit', displayStats);
